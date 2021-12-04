ac3a73505c830bcc8fe0662a5955b6b54dfe4f8f
Merkaber <social.pio@posteo.jp>
2021-12-04 15:05:27 +0100

    Changed file names

--- library.h
+++
@@ -1,6 +0,0 @@
-#ifndef QUICKSQLITE_LIBRARY_H
-#define QUICKSQLITE_LIBRARY_H
-
-void hello();
-
-#endif //QUICKSQLITE_LIBRARY_H
--- src/quicksqlite.cpp
+++ src/quicksqlite.cpp
@@ -1,4 +1,4 @@
-#include "library.h"
+#include "quicksqlite.h"
 
 #include <iostream>
 
