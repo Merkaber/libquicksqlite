32a47b1dd14a45952a7e3096e6840ea42b025c10
Merkaber <social.pio@posteo.jp>
2021-12-04 16:26:42 +0100

    Implemented first stub class for database access. Open function still needs to be implemented fully.

--- include/quicksqlite.h
+++ include/quicksqlite.h
@@ -1,3 +1,63 @@
+/*
+ * quicksqlite
+ *
+ * Copyright (c) 2021 Simon Piorecki
+ */
+
 #pragma once
 
-void hello();
+#include <sqlite3.h>
+#include <exception>
+
+namespace quicksqlite {
+    class Exception : std::exception {
+    public:
+        Exception(const char* error_msg, int error);
+        [[nodiscard]] const char* what() const noexcept override;
+        [[nodiscard]] const int& get_error() const noexcept;
+
+    private:
+        const char* const error_msg;
+        const int sqlite_err_code;
+    };
+
+    class Database {
+
+    /* Interface */
+    public:
+
+        /**
+         * Scott Meyers' singleton
+         */
+        Database(const Database&) = delete;
+        void operator=(const Database&) = delete;
+
+        /**
+         * @return The unique instance of this class
+         */
+        static Database& instance() noexcept;
+
+        /**
+         * Opens the database if not already done
+         * @param filepath The path to the database file
+         * @return True if a new database has been opened, false if a database is already open
+         * @throw \ref quicksqlite::Exception if an error occurred
+         */
+        bool open(const char* filepath) noexcept(false);
+
+    /* Member functions */
+    private:
+
+        /**
+         * Scott Meyers' singleton
+         */
+        Database() = default;
+
+    /* Member */
+    private:
+
+        /// Flag which indicates that a proper database has already been opened
+        bool is_open = false;
+
+    };
+}
--- src/quicksqlite.cpp
+++ src/quicksqlite.cpp
@@ -1,7 +1,48 @@
+/*
+ * quicksqlite
+ *
+ * Copyright (c) 2021 Simon Piorecki
+ */
+
 #include "quicksqlite.h"
 
-#include <iostream>
+#include "Tools.h"
 
-void hello() {
-    std::cout << "Hello, World!" << std::endl;
+quicksqlite::Exception::Exception(const char* const error_msg, const int error) : error_msg(error_msg), sqlite_err_code(error)
+{
+
+}
+
+const char* quicksqlite::Exception::what() const noexcept
+{
+    return error_msg;
+}
+
+const int& quicksqlite::Exception::get_error() const noexcept
+{
+    return sqlite_err_code;
+}
+
+quicksqlite::Database& quicksqlite::Database::instance() noexcept
+{
+    static quicksqlite::Database database;
+    return database;
+}
+
+bool quicksqlite::Database::open(const char* filepath) noexcept(false)
+{
+    if (is_open) {
+        return false;
+    }
+
+    /* Create new database with the given file path if the database did not exist yet */
+    if (!Tools::file_exists(filepath)) {
+        //TODO: Create database
+        return true;
+    }
+
+
+
+    is_open = true;
+    return true;
 }
